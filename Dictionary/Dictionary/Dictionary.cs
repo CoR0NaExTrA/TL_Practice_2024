9d889f1c3563b4cef39be070019cdee867f91983
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-08-09 11:30:24 +0300

    Complete dictionary homework

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,121 @@
+﻿bool usingDictionary = true;
+Dictionary<string, string> wordsDictionary = new();
+wordsDictionary = LoadDictionaryFromFile( "Dictionary.txt" );
+
+Console.WriteLine( "Dictionary" );
+PrintCommands();
+
+while ( usingDictionary )
+{
+    Console.Write( "Введите команду: " );
+    switch ( Console.ReadLine().Trim().ToLower() )
+    {
+        case "exit":
+            Console.WriteLine( "Завершаем работу и сохраняем словарь..." );
+            usingDictionary = false;
+            break;
+        case "addtranslation":
+            AddTranslation( wordsDictionary );
+            break;
+        case "translate":
+            TryTranslate( wordsDictionary );
+            break;
+        default:
+            Console.WriteLine( "Неизвестная команда!" );
+            PrintCommands();
+            break;
+    }
+}
+SaveDictionaryToFile( wordsDictionary );
+
+Dictionary<string, string> LoadDictionaryFromFile( string filePath )
+{
+    Dictionary<string, string> dictionary = new();
+    try
+    {
+        StreamReader sr = new StreamReader( filePath );
+        string line;
+        while ( ( line = sr.ReadLine() ) != null )
+        {
+            string[] words = line.Split( ":" );
+            if ( ( words[ 0 ] != null ) && ( words[ 1 ] != null ) )
+            {
+                dictionary.Add( words[ 0 ], words[ 1 ] );
+            }
+        }
+        sr.Close();
+    }
+    catch ( Exception e )
+    {
+        Console.WriteLine( $"Ошибка при работе с файлом: {e.Message}" );
+    }
+    return dictionary;
+}
+
+static string GetNotEmptyString( string value )
+{
+    Console.Write( $"Введите {value}: " );
+    string str = Console.ReadLine();
+    while ( string.IsNullOrWhiteSpace( str ) )
+    {
+        Console.WriteLine( "Поле не должно быть пустым, введите его еще раз" );
+        str = Console.ReadLine();
+    }
+    return str;
+}
+
+void AddTranslation( Dictionary<string, string> dictionary )
+{
+    string word = GetNotEmptyString( "слово" );
+    if ( dictionary.ContainsKey( word ) )
+    {
+        Console.WriteLine( $"Упс! Слово \"{word}\" уже есть в словаре!" );
+        Console.WriteLine( $"Используйте команду \"translate\" чтобы увидеть его перевод" );
+    }
+    else
+    {
+        string translation = GetNotEmptyString( "перевод слова" );
+        dictionary.Add( word, translation );
+    }
+}
+
+void TryTranslate( Dictionary<string, string> d )
+{
+    const string positiveAnser = "y";
+    string word = GetNotEmptyString( "слово" );
+    if ( d.ContainsKey( word ) )
+    {
+        Console.WriteLine( $"Слово \"{word}\" переводится как \"{d[ word ]}\"" );
+    }
+    else
+    {
+        Console.Write( $"Слова \"{word}\" нет в словаре, хотите добавить перевод({positiveAnser}/n): " );
+        if ( Console.ReadLine() == positiveAnser )
+        {
+            string translation = GetNotEmptyString( "перевод слова" );
+            d.Add( word, translation );
+        }
+    }
+}
+
+void SaveDictionaryToFile( Dictionary<string, string> d )
+{
+    if ( File.Exists( "Dictionary.txt" ) )
+    {
+        File.Delete( "Dictionary.txt" );
+    }
+
+    StreamWriter sw = new StreamWriter( "Dictionary.txt" );
+    foreach ( var pair in d )
+    {
+        sw.WriteLine( $"{pair.Key}{":"}{pair.Value}" );
+    }
+    sw.Close();
+}
+
+void PrintCommands()
+{
+    Console.WriteLine( "translate - перевести слово" );
+    Console.WriteLine( "addTranslation - добавить перевод" );
+    Console.WriteLine( "exit - выход" );
+}
--- Dictionary/Dictionary/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Dictionary");
-Console.Write("Enter word for tranlation: ");
-string wordUser = Console.ReadLine();
-
-File.ReadAllLines()
