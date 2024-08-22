c930ec31c6f4a6b2811e937c8324f7d26432ba7d
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-08-22 09:24:46 +0300

    Исправил условия для 5-ой задачи

--- SQL/SQLQuery4.sql
+++ SQL/SQLQuery4.sql
@@ -1,12 +1,9 @@
-/* ¬се доступные номера сегодн€ */
 SELECT room_number FROM dbo.Rooms
 WHERE availability = 'free';
 
-/* ¬се клиенты, фамили€ которых начинаетс€ на S */
 SELECT last_name, first_name FROM dbo.Customers
 WHERE last_name LIKE 'S%';
 
-/* ѕоиск бронировани€ дл€ определенного клиента(по имени или email) */
 SELECT C.first_name, C.last_name, R.room_number, B.check_in_date, B.check_out_date
 FROM dbo.Bookings AS B
 JOIN dbo.Customers AS C
@@ -15,7 +12,6 @@ JOIN dbo.Rooms AS R
 ON B.room_id = R.room_id
 WHERE first_name = 'Edna' OR email = 'ednall@example.com';
 
-/* ѕоиск бронировани€ дл€ определенного номера */
 SELECT R.room_number, C.first_name, C.last_name, B.check_in_date, B.check_out_date
 FROM dbo.Bookings AS B
 JOIN dbo.Customers AS C
@@ -24,9 +20,10 @@ JOIN dbo.Rooms AS R
 ON B.room_id = R.room_id
 WHERE room_number = '505';
 
-/* ¬се номера незабронированные на определленцю дату */
 SELECT R.room_number
-FROM dbo.Bookings AS B
-RIGHT JOIN dbo.Rooms AS R
-ON B.room_id = R.room_id
-WHERE check_in_date != '06/09/2024' OR availability = 'true';
+FROM dbo.Rooms AS R
+WHERE availability = 'free' AND R.room_id NOT IN (
+		SELECT B.room_id
+		FROM dbo.Bookings AS B
+		WHERE '01/11/2024' BETWEEN B.check_in_date AND B.check_out_date
+);
